0eea813f2b84ef18eeaf1e9a32b2cfe69fb4e9fc
RoxyBadmosh <aminabibisamrat@gmail.com>
2025-08-20 19:17:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityName: "YukaXmusic", 
+  activityType: "PLAYING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/cRWQKD4VDA",
   embedTimeout: 5, 
   errorLog: "", 
